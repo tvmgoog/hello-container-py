db787cf69cae402c5eb1b6e08c81dc27f56f4536
tvmgoog <64491000+tvmgoog@users.noreply.github.com>
2022-01-14 12:43:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from inside the changed application!"
+    return "Hello World, from inside the changed application! from QL "
 
 
 if __name__ == "__main__":
